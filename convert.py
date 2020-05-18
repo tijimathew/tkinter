7ea46b1f2e1b3834b4e675f31a1e3b5f118fb9b7
Tiji Mathew <8884797+tijimathew@users.noreply.github.com>
2020-05-17 21:17:14 -0400

    added text and label to convert kg to other units

--- convert.py
+++ convert.py
@@ -2,13 +2,44 @@ from tkinter import *
 
 window = Tk()
 
-ent_value = Entry(window)
-ent_value.grid(row = 0, column = 0)
+def convert_kg_to_gm(value):
+    if type(value) == float:
+        return value * 1000.0
+    else:
+        return float(value) * 1000.0
+
+def convert_kg_to_lb(value):
+    if type(value) == float:
+        return value * 2.20462
+    else:
+        return float(value) * 2.20462
+
+def convert_kg_to_oz(value):
+    if type(value) == float:
+        return value * 35.2739199982575
+    else:
+        return float(value) * 35.2739199982575
+
+
+def convert_units():
+    input_value = float(ent_field_value.get())
+
+    txt_grams.delete("1.0", END)
+    txt_grams.insert(END, str(convert_kg_to_gm(input_value)))
+    txt_pounds.delete("1.0", END)
+    txt_pounds.insert(END, str(convert_kg_to_lb(input_value)))
+    txt_ounces.delete("1.0", END)
+    txt_ounces.insert(END, str(convert_kg_to_oz(input_value)))
+
+
+ent_field_value = StringVar()
+ent_field = Entry(window, textvariable = ent_field_value)
+ent_field.grid(row = 0, column = 0)
 
 lbl_unit = Label(window, text = "Kg")
 lbl_unit.grid(row = 0, column = 1)
 
-btn_convert = Button(window, text="Convert")
+btn_convert = Button(window, text="Convert", command = convert_units)
 btn_convert.grid(row = 0, column = 2)
 
 txt_grams = Text(window, height = 1, width = 20)
